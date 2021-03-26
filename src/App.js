eabfea6dfd648a7157a8c68a33819cb381073911
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-03-26 13:21:44 -0300

    Slick Carousel config

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import Header from './components/Header/Header';
 import Gallery from './components/Gallery/Gallery';
+import Slick from './components/Slick/Slick';
 
 function App() {
   return (
     <div className="App container-flex">
       <Header />
       <Gallery />
+      <Slick />
     </div>
   );
 }
