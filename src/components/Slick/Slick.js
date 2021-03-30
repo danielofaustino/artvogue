6b9777d4d096d2004313989edba0834d5d315c79
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-03-30 12:09:53 -0300

    Services loop

--- src/components/Slick/Slick.js
+++ src/components/Slick/Slick.js
@@ -2,9 +2,60 @@ import { Container } from 'reactstrap';
 
 import React from 'react';
 import Slider from 'react-slick';
+
+// importing assets
+import Revestimentos from '../../assets/services/revestimentos.svg';
 import Eletrica from '../../assets/services/eletrica.svg';
+import Pintura from '../../assets/services/pintura.svg';
+import Hidraulica from '../../assets/services/hidraulica.svg';
+import Iluminacao from '../../assets/services/iluminacao.svg';
 
 export default function Slick() {
+  const services = [
+    {
+      title: 'Revestimentos',
+      src: Revestimentos,
+      alt: 'Revestimentos',
+      href: '/',
+    },
+    {
+      title: 'Elétrica',
+      src: Eletrica,
+      alt: 'Elétrica',
+      href: '/',
+    },
+    {
+      title: 'Pintura',
+      src: Pintura,
+      alt: 'Pintura',
+      href: '/',
+    },
+    {
+      title: 'Hidraulica',
+      src: Hidraulica,
+      alt: 'Hidraulica',
+      href: '/',
+    },
+    {
+      title: 'Iluminação',
+      src: Iluminacao,
+      alt: 'Iluminação',
+      href: '/',
+    },
+    {
+      title: 'Eletrica',
+      src: Eletrica,
+      alt: 'Eletrica',
+      href: '/',
+    },
+    {
+      title: 'Eletrica',
+      src: Eletrica,
+      alt: 'Eletrica',
+      href: '/',
+    },
+  ];
+
   const settings = {
     dots: true,
     infinite: false,
@@ -44,90 +95,25 @@ export default function Slick() {
   return (
     <Container className="container my-5" id="services">
       <Slider {...settings}>
-        <div>
-          <div className="services-item">
-            <a href="/" className="destaque">
-              <div className="img-container">
-                <img src={Eletrica} alt="eletica" className="mx-auto d-block" />
-              </div>
+        {services.map((x) => (
+          <div>
+            <div className="services-item">
+              <a href={x.href} className="destaque">
+                <div className="img-container">
+                  <img
+                    src={x.src}
+                    alt={x.alt}
+                    Style="height:109px;width:120px"
+                    className="mx-auto d-block"
+                  />
+                </div>
+              </a>
+            </div>
+            <a href="/" className="destaque_title">
+              {x.title.toUpperCase()}
             </a>
           </div>
-          <a href="/" className="destaque_title">
-            ELETRICA
-          </a>
-        </div>
-        <div>
-          <div className="services-item">
-            <a href="/" className="destaque">
-              <div className="img-container">
-                <img src={Eletrica} alt="eletica" className="mx-auto d-block" />
-              </div>
-            </a>
-          </div>
-          <a href="/" className="destaque_title">
-            ELETRICA
-          </a>
-        </div>
-        <div>
-          <div className="services-item">
-            <a href="/" className="destaque">
-              <div className="img-container">
-                <img src={Eletrica} alt="eletica" className="mx-auto d-block" />
-              </div>
-            </a>
-          </div>
-          <a href="/" className="destaque_title">
-            ELETRICA
-          </a>
-        </div>
-        <div>
-          <div className="services-item">
-            <a href="/" className="destaque">
-              <div className="img-container">
-                <img src={Eletrica} alt="eletica" className="mx-auto d-block" />
-              </div>
-            </a>
-          </div>
-          <a href="/" className="destaque_title">
-            ELETRICA
-          </a>
-        </div>
-        <div>
-          <div className="services-item">
-            <a href="/" className="destaque">
-              <div className="img-container">
-                <img src={Eletrica} alt="eletica" className="mx-auto d-block" />
-              </div>
-            </a>
-          </div>
-          <a href="/" className="destaque_title">
-            ELETRICA
-          </a>
-        </div>
-        <div>
-          <div className="services-item">
-            <a href="/" className="destaque">
-              <div className="img-container">
-                <img src={Eletrica} alt="eletica" className="mx-auto d-block" />
-              </div>
-            </a>
-          </div>
-          <a href="/" className="destaque_title">
-            ELETRICA
-          </a>
-        </div>
-        <div>
-          <div className="services-item">
-            <a href="/" className="destaque">
-              <div className="img-container">
-                <img src={Eletrica} alt="eletica" className="mx-auto d-block" />
-              </div>
-            </a>
-          </div>
-          <a href="/" className="destaque_title">
-            ELETRICA
-          </a>
-        </div>
+        ))}
       </Slider>
     </Container>
   );
