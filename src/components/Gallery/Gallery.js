4774e36eb1ea7d024e3a8345840737e3ea57da83
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-04-01 12:22:48 -0300

    Partners and Quotation forms

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,24 +1,125 @@
-import React from 'react';
-import { Container, Row, Col, Button } from 'reactstrap';
+import { React, useState } from 'react';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Container,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from 'reactstrap';
 import Carousell from '../Carousel/Carousell';
 
-const Gallery = () => (
-  <Container id="gallery" className="mt-2">
-    <Row>
-      <Col className="content-box " xl="5">
-        <h1 className="display-4">SERVIÇOS</h1>
-        <h1 className="display-4">OBRAS</h1>
-        <h1 className="display-4">E REFORMAS</h1>
+export default function Gallery() {
+  const [modal, setModal] = useState(false);
 
-        <Button color="secondary">Orçamentos</Button>
+  const toggle = () => setModal(!modal);
 
-        <Button color="secondary">Portifólio</Button>
-      </Col>
-      <Col className="align-self-center" xl="7">
-        <Carousell />
-      </Col>
-    </Row>
-  </Container>
-);
+  return (
+    <Container id="gallery" className="mt-2">
+      <Row>
+        <Col className="content-box " xl="5">
+          <h1 className="display-4">SERVIÇOS</h1>
+          <h1 className="display-4">OBRAS</h1>
+          <h1 className="display-4">E REFORMAS</h1>
 
-export default Gallery;
+          <Button color="secondary" onClick={toggle}>
+            Orçamentos
+          </Button>
+
+          <Button color="secondary">@artvogueoficial</Button>
+        </Col>
+        <Col className="align-self-center" xl="7">
+          <Carousell />
+        </Col>
+      </Row>
+      <Modal isOpen={modal} toggle={toggle} className={modal}>
+        <ModalHeader toggle={toggle}>Orçamentos</ModalHeader>
+        <ModalBody>
+          <Form
+            className="artvogue-forms"
+            action="https://docs.google.com/forms/d/e/1FAIpQLSdkPgmpQG_ylq5bGCCfZUWHqsFI8J7CGAtMGIC_7DdNczGpMw/formResponse"
+            target="_self"
+            id="bootstrapForm"
+            method="POST"
+          >
+            <FormGroup>
+              <Label for="732073748">Nome:</Label>
+              <Input
+                id="175199926"
+                type="text"
+                name="entry.175199926"
+                class="form-control"
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="836740015">Celular:</Label>
+              <Input
+                id="483006589"
+                type="text"
+                name="entry.483006589"
+                class="form-control"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="409924997">Endereço:</Label>
+              <Input
+                id="1422977614"
+                name="entry.1422977614"
+                class="form-control"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleSelectMulti">Serviços</Label>
+              <Input
+                type="select"
+                multiple
+                id="1795896370"
+                name="entry.801317577"
+                class="form-control"
+              >
+                <option value="Climatização">Climatização</option>
+                <option value="Elétrica">Elétrica</option>
+                <option value="Iluminação">Iluminação</option>
+                <option value="Hidráulica">Hidráulica</option>
+                <option value="Pintura">Pintura</option>
+                <option value="Drywall">Drywall</option>
+                <option value="Obra">Obra</option>
+                <option value="Vidros e Espelhos">Vidros e Espelhos</option>
+                <option value="Revestimentos">Revestimentos</option>
+                <option value="Marmoraria">Marmoraria</option>
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="1791839778">Descreva a sua necessidade:</Label>
+              <Input
+                type="textarea"
+                id="979805659"
+                name="entry.979805659"
+                class="form-control"
+                required
+              />
+            </FormGroup>
+            <Input type="hidden" name="fvv" value="1" />
+            <Input type="hidden" name="fbzx" value="-4638860087270209793" />
+
+            <Input type="hidden" name="pageHistory" value="0" />
+
+            <Input class="btn btn-primary" type="submit" value="Enviar" />
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggle}>
+            Fechar
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </Container>
+  );
+}
--- src/components/Quotation/Quotation.js
+++ src/components/Quotation/Quotation.js
@@ -1,20 +1,146 @@
-import React from 'react';
-import { Container, Row, Col, Jumbotron, Button } from 'reactstrap';
+import React, { useState } from 'react';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Container,
+  Row,
+  Col,
+  Jumbotron,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from 'reactstrap';
+
 import logo from '../../assets/logo3.png';
 
 export default function Quotation() {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
   return (
     <Container className="mt-5">
       <Row>
         <Col cl={12}>
           <Jumbotron id="quotation">
             <img src={logo} alt="logo" height="68px" />
-            <h3>SOLICITE UM ORÇAMENTO</h3>
+            <h3>Seja nosso Parceiro</h3>
 
             <p>
-              <Button variant="primary">Orçamento</Button>
+              <Button variant="primary" onClick={toggle}>
+                Cadastro - Parceiros
+              </Button>
             </p>
           </Jumbotron>
+
+          <Modal isOpen={modal} toggle={toggle} className={modal}>
+            <ModalHeader toggle={toggle}>Parceiros</ModalHeader>
+            <ModalBody>
+              <Form
+                className="artvogue-forms"
+                action="https://docs.google.com/forms/d/e/1FAIpQLSf5OxaWHAJ5mhYXpMUWrUcbitDa_-RCYdAWllh7N7KfnN9TUQ/formResponse"
+                target="_self"
+                id="bootstrapForm"
+                method="POST"
+              >
+                <FormGroup>
+                  <Label for="732073748">Nome:</Label>
+                  <Input
+                    id="175199926"
+                    type="text"
+                    name="entry.175199926"
+                    class="form-control"
+                    required
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="836740015">Celular:</Label>
+                  <Input
+                    id="483006589"
+                    type="text"
+                    name="entry.483006589"
+                    class="form-control"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="1795896370">Especialidades:</Label>
+                  <Input
+                    id="801317577"
+                    name="entry.801317577"
+                    class="form-control"
+                    required
+                  />
+                </FormGroup>
+
+                <FormGroup tag="fieldset">
+                  <legend htmlFor="511292062">Região de Atendimento:</legend>
+                  <FormGroup check>
+                    <Label check>
+                      <Input
+                        type="radio"
+                        name="entry.946292988"
+                        value="São Paulo - Norte"
+                      />{' '}
+                      São Paulo - Norte
+                    </Label>
+                  </FormGroup>
+                  <FormGroup check>
+                    <Label check>
+                      <Input
+                        type="radio"
+                        name="entry.946292988"
+                        value="São Paulo - Sudeste"
+                      />{' '}
+                      São Paulo - Sudeste
+                    </Label>
+                  </FormGroup>
+                  <FormGroup check>
+                    <Label check>
+                      <Input
+                        type="radio"
+                        name="entry.946292988"
+                        value="São Paulo - Sudoeste"
+                      />{' '}
+                      São Paulo - Sudoeste
+                    </Label>
+                  </FormGroup>
+                  <FormGroup check>
+                    <Label check>
+                      <Input
+                        type="radio"
+                        name="entry.946292988"
+                        value="São Paulo - Oeste"
+                      />{' '}
+                      São Paulo - Oeste
+                    </Label>
+                  </FormGroup>
+                </FormGroup>
+
+                <FormGroup>
+                  <Label for="334710371">Fale mais sobre você:</Label>
+                  <Input
+                    id="999199171"
+                    name="entry.999199171"
+                    class="form-control"
+                  />
+                </FormGroup>
+                <input type="hidden" name="fvv" value="1" />
+                <input type="hidden" name="fbzx" value="1277207772728743201" />
+
+                <Input type="hidden" name="pageHistory" value="0" />
+
+                <Input class="btn btn-primary" type="submit" value="Enviar" />
+              </Form>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="secondary" onClick={toggle}>
+                Fechar
+              </Button>
+            </ModalFooter>
+          </Modal>
         </Col>
       </Row>
     </Container>
