c3283ba018bd55cf0e5dff2a05430a2ece86b8f9
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-04-05 11:24:38 -0300

    cookie-consent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import React from 'react';
+import CookieConsent from 'react-cookie-consent';
 import Home from './Pages/Home';
 import Contact from './Pages/Contact';
 
@@ -18,6 +19,30 @@ function App() {
           <Route path="/contatos" exact component={Contact} />
         </Switch>
       </Router>
+      <CookieConsent
+        location="bottom"
+        buttonText="ACEITAR"
+        enableDeclineButton
+        cookieName="myAwesomeCookieName2"
+        style={{ background: '#FFF', color: '#244479' }}
+        buttonStyle={{
+          background: '#3968b3',
+          color: '#FFF',
+          fontSize: '13px',
+          borderRadius: '10px',
+        }}
+        declineButtonStyle={{
+          background: '#fe914e',
+          color: '#FFF',
+          fontSize: '13px',
+          borderRadius: '10px',
+        }}
+        declineButtonText="NÃO ACEITAR"
+        expires={150}
+      >
+        Nós usamos cookies e outras tecnologias semelhantes para melhorar a sua
+        experiência em nosso site.
+      </CookieConsent>
     </div>
   );
 }
